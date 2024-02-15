22fa8e66897d13563ac33866c2c17cfbfd7622dc
SUJI BAE <business.thetititle@gmail.com>
2024-02-15 22:38:31 +0900

    productPage delete

--- quasar/src/router/routes.js
+++ quasar/src/router/routes.js
@@ -4,7 +4,6 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'product', component: () => import('pages/ProductPage.vue') }
     ]
   },
 
