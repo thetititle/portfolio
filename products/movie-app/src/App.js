83dfdc8f9f053459d0bb0381c8c6835d3fd4b158
thetititle@gmail.com <business.thetititle@gmail.com>
2024-02-23 14:36:06 +0900

    Page componet 생성

--- products/movie-app/src/App.js
+++ products/movie-app/src/App.js
@@ -2,40 +2,6 @@ import { useState, useEffect } from 'react';
 import Movie from './components/Movie';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
-      )
-    ).json();
-    const movies = json.data.movies;
-    setMovies(movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log('movies', movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>now Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              propCover_image={movie.medium_cover_image}
-              propTitle={movie.title}
-              propSummary={movie.summary}
-              propGenres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 export default App;
---
+++ products/movie-app/src/routes/PageDetail.js
@@ -0,0 +1,4 @@
+function PageDetail() {
+  return <h1>디테일 페이!지</h1>;
+}
+export default PageDetail;
